0b1b5366be575ec0f4f7da59b0b62c0a6ff3bd28
triardn <tr.ardn@gmail.com>
2016-03-13 15:01:42 +0700

    navigation header

--- functions.php
+++ functions.php
@@ -7,3 +7,13 @@ define('site_url', site_url());
 
 // Add Themes Support
 add_theme_support('menus');
+add_theme_support('post-thumbnails');
+
+$custom_header = array(
+		'width' => 2992,
+		'height' => 2000,
+		'default-image' => dwp_theme_dir . '/assets/img/dwp.jpg',
+		'header-text' => false
+	);
+
+add_theme_support('custom-header', $custom_header);
